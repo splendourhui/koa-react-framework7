d7a9a1ca3e1fd3e43a1982d0ad52d144f7cf326b
郑南辉 <zhengnanhui@cvte.com>
2015-09-25 15:55:38 +0800

    [Other] Remove unused files.

--- routes/pages/index.js
+++ routes/pages/index.js
@@ -2,7 +2,6 @@
 "use strict";
 
 const ctrl = require('./controller');
-const wechat_verification = require('../../middlewares/wechat_verification');
 
 module.exports = (router) => {
   router.get('/', ctrl.index);
